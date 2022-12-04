abe8c08a70ffc4a6fd6878857413fde8ec632f81
Bibek Poudel <105146448+bibekpoudel09@users.noreply.github.com>
2022-12-04 14:57:06 +0545

    Create ForLoops.java

---
+++ LoopsInJava/ForLoops.java
@@ -0,0 +1,15 @@
+/*
+ * for (initialization; condition; updation){]
+ *  we initialize the value with a variable with declared datatype then we 
+ *  define a termination condition and at last we define a updating condition for a loop.   
+ */
+
+
+
+public class ForLoops{
+    public static void main(String[] args){
+        for (int counter = 0; counter<100; counter = counter + 2){ 
+            System.out.println("God is Dead.");
+        }
+    }
+}
