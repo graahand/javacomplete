cdabe904fcc12e6e574ce7e57acd95302e1007f6
Bibek Poudel <105146448+bibekpoudel09@users.noreply.github.com>
2022-12-04 14:51:41 +0545

    Create HelloWorld.java

---
+++ Lab2/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * We are printing Hello World! using java.
+ * @author Bibek Poudel
+ * version 1.1
+ */
+
+
+
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
